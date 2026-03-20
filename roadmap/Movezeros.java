72104a10e4223c6e07929721c14be70aea0afe22
s240224 <s240224@th-ab.de>
2026-03-20 11:24:29 +0100

    bound

--- roadmap/Movezeros.java
+++ roadmap/Movezeros.java
@@ -7,21 +7,17 @@ public class Movezeros {
         int slow = 0;
 
         for (int fast = 0; fast < arr.length; fast++) {
-
-            if (arr[fast] != 0) {
+            if(arr[fast] != 0) {
                 int temp = arr[slow];
                 arr[slow] = arr[fast];
                 arr[fast] = temp;
 
                 slow++;
             }
-
         }
-
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
 
     }
-
 }
---
+++ roadmap/bounds.java
@@ -0,0 +1,49 @@
+package roadmap;
+
+public class bounds {
+    public static void main(String[] args) {
+    }
+
+    public int lowerbound(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = arr.length;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+
+        }
+        return ans;
+    }
+
+    public int upperbound(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+        int ans = arr.length;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] > target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public int countOccurrences(int[] arr, int target) {
+        int lb = lowerbound(arr, target);
+        int ub = upperbound(arr, target);
+        int count = ub - lb;
+        return count;
+    }
+}
