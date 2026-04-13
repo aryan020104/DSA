668f417f92fdeb0175e56448c79d7f6b4c44db0c
s240224 <s240224@th-ab.de>
2026-04-13 16:12:56 +0200

    Stack

---
+++ roadmap/DailyTemp.java
@@ -0,0 +1,28 @@
+package roadmap;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class DailyTemp {
+    public static int[] DistanceTemp(int[] arr) {
+        int[] temp = arr;
+        int[] ans = new int[temp.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < temp.length; i++) {
+            while (!stack.isEmpty() && temp[i] > temp[stack.peek()]) {
+                int prev = stack.pop();
+                ans[prev] = i - prev;
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 73, 74, 75, 71, 69, 72, 76, 73 };
+        int[] finaltemp = DistanceTemp(arr);
+
+        System.out.println(Arrays.toString(finaltemp));
+    }
+}
--- roadmap/NSE.java
+++ roadmap/NSE.java
@@ -1,24 +1,22 @@
 package roadmap;
 
-import java.util.Arrays; 
+import java.util.Arrays;
 import java.util.Stack;
 
-public class NGE {
-    public static int[] nextGreater(int[] arr){
+public class NSE {
+    public static int[] SmallNum(int[] arr) {
         int n = arr.length;
         int[] result = new int[n];
         Stack<Integer> stack = new Stack<>();
 
+        for (int i = n - 1; i >= 0; i--) {
 
-        for(int i = n-1; i>=0; i--){
-
-            while(!stack.isEmpty() && stack.peek() <= arr[i]){
+            while (!stack.isEmpty() && stack.peek() >= arr[i])
                 stack.pop();
-            }
 
-            if(stack.isEmpty()){
+            if (stack.isEmpty()) {
                 result[i] = -1;
-            }else{
+            } else {
                 result[i] = stack.peek();
             }
             stack.push(arr[i]);
@@ -26,11 +24,12 @@ public class NGE {
         return result;
 
     }
-    public static void main(String[] args) {
-        int[] arr = {2,1,2,4,3};
-        int[] res = nextGreater(arr);
 
-        System.out.println(Arrays.toString(res));
+    public static void main(String[] args) {
+        int[] arr = { 4, 8, 5, 2, 10 };
+        int[] Smallelement = SmallNum(arr);
+
+        System.out.println(Arrays.toString (Smallelement));
     }
-    
+
 }
